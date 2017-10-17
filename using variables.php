02df192e625171a5818bbc98e26188d4b449cbbb
Michelle Jacobs <32098753+JacobsMBPCMagenta@users.noreply.github.com>
2017-10-17 14:19:19 +0100

    Add files via upload

---
+++ using variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First PHP Page</h1>
+
+<?php
+$myfirst = "Hello world ofcourse!";
+$firstNum = 6;
+$firstFloat =12.5
+.
+echo $myfirst;
+echo "<br>";
+echo $firstNum;
+echo "<br>";
+echo $firstFloat;
+?>
+
+</body>
+</html>
